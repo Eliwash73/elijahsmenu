e4b29058f5f4ecd4503da259984c0d7adbd53130
EliWash73 <Washingtonelijah73@gmail.com>
2025-04-27 00:04:06 -0400

    init

---
+++ src/main/java/com/ejwashin/elijahsmenu/RecipeApp.java
@@ -0,0 +1,13 @@
+package com.ejwashin.elijahsmenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeApp.class, args);
+	}
+
+}
